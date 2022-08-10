8e11eb496373153957afb81c8af8c54e523071a7
Darren <splicereborn@gmail.com>
2022-08-10 21:20:07 +0800

    3.8*: Phonebook backend step8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,7 +3,10 @@ const app = express()
 app.use(express.json())
 
 var morgan = require('morgan')
-app.use(morgan('tiny'))
+morgan.token('body', (req) => {
+    return JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     { 
