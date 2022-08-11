4f3f31ac4b999f7a50713bac686d39380d9c9db4
Darren <splicereborn@gmail.com>
2022-08-12 00:48:05 +0800

    3.16: Phonebook db, step4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,15 +2,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const Person = require('./models/person')
+
+app.use(express.static('build'))
 app.use(express.json())
 
 const cors = require('cors')
 app.use(cors())
 
-app.use(express.static('build'))
-
 var morgan = require('morgan')
-const { response } = require('express')
 morgan.token('body', (req) => {
     return JSON.stringify(req.body)
 })
@@ -28,37 +27,41 @@ app.get('/info', (req, res) => {
     `)
 })
 
-app.get('/api/persons', (req, res) => {
+app.get('/api/persons', (req, res, next) => {
     Person.find({})
         .then(persons => res.json(persons))
+        .catch(err => next(err))
 })
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
     Person.findById(req.params.id)
         .then(person => {
             if (person) res.json(person)
-            else res.status(404).end()
+            else {
+                const e = new Error('Id not found')
+                e.name = 'idNull'
+                next(e)
+            }
         })
         .catch(error => {
-            console.log(error)
-            res.status(400).send({error: 'malformatted id'})
+            next(error)
         })
 })
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
     Person.findByIdAndDelete(req.params.id)
         .then(result => {
             res.json(204).end()
         })
-        .catch(error => console.log(error))
+        .catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
     // name & number cannot be empty!
     if (!req.body.name || !req.body.number) {
-        return res.status(400).json({
-            error: 'name and number must be filled'
-        })
+        const e = new Error('name and number must be filled')
+        e.name = 'NameNumberEmpty'
+        return next(e)
     }
     /* 3.14: Temporary allow adding repeated names
      * const nameExists = persons.filter(p => 
@@ -78,3 +81,16 @@ app.post('/api/persons', (req, res) => {
     newPerson.save()
         .then(savedPerson => res.json(savedPerson))
 })
+
+const errorHandler = (err, req, res, next) => {
+    if (err.name === 'CastError') {
+        return res.status(400).send({error: 'malformatted id'})
+    } else if (err.name === 'NameNumberEmpty') {
+        return res.status(400).send({error: err.message})
+    } else if (err.name === 'idNull') {
+        return (res.status(404).send({error: err.message}))
+    }
+    next(err)
+}
+
+app.use(errorHandler)
